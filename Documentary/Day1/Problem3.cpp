e50ba2375dbf402e3a81d1ed6d34485fc421eac9
one-blip <22bcs17257@cuchd.in>
2024-12-19 18:43:19 +0530

    Create Problem3.cpp

---
+++ Documentary/Day1/Problem3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Print your whole Academic bio data: Name, UID, Section, Area of Interest, Age, last year CGPA and more
+
+int main()
+{
+    cout << "Name : Ravinder Sehrawat" << endl;
+    cout << "UID : 22BCS17257" << endl;
+    cout << "Section : IOT_630 A" << endl;
+    cout << "Area of interest: Technology" << endl;
+    cout << "Age : 23" << endl;
+    cout << "Last Year CGPA" << endl;
+    cout << "DOB : 11/06/2003" << endl;
+    cout << "Home Town : Palwal, Haryana" << endl;
+
+    return 0;
+}
