3e307cc276638289e120361c53931651a9e8864e
one-blip <22bcs17257@cuchd.in>
2024-12-26 12:18:02 +0530

    Create problem1.cpp

---
+++ Documentary/Day3/problem1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* current = dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+        int sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum / 10;
+        current->next = new ListNode(sum % 10);
+        current = current->next;
+    }
+
+    return dummy->next;
+}
+
+int main() {
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode* result = addTwoNumbers(l1, l2);
+
+    while (result) {
+        cout << result->val << " ";
+        result = result->next;
+    }
+
+    return 0;
+}
